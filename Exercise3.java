acc245379e8c34dd5a21adfef3ea4966edf909d0
StanislavAlpatiev <alpatiev.stanislav@gmail.com>
2021-04-13 14:41:20 +0200

    bugg med split

--- Exercise3.java
+++ Exercise3.java
@@ -1,8 +1,5 @@
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class Exercise3 {
 
@@ -12,14 +9,63 @@ public class Exercise3 {
 	//exporterar det som finns i listan till filen som ges som
 	//parameter enligt formatet för exportfilen (se nästa sektion).
 	public void exportRecordings(String fileName) {
+		try {
+			FileWriter utfil = new FileWriter(fileName + ".txt");
+			PrintWriter out = new PrintWriter(utfil);
 
+			for (Recording recording : recordings) {
+				out.println("<recording>");
+				out.println("	<artist>" + recording.getArtist() + "</artist>");
+				out.println("	<title>" + recording.getTitle() + "</title>");
+				out.println("	<year>" + recording.getYear() + "</year>");
+				out.println("	<genres>");
+				ArrayList<String> genres = new ArrayList<String>(recording.getGenre());
+				for (String genre : genres) {
+					out.println("		<genre>" + genre + "</genre>");
+				}
+				out.println("	</genres>");
+				out.println("</recording>");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	//importerar det som finns i filen (som är i formatet för
 	//importfilen, se nästa sektion) som ges som parameter till listan. Om filen inte
 	//finns ska ett undantag av typen FileNotFoundException genereras.
 	public void importRecordings(String fileName) throws FileNotFoundException {
+		try {
+			FileReader reader = new FileReader(fileName + ".txt");
+			BufferedReader in = new BufferedReader(reader);
+			String line;
+			String title = "";
+			String artist = "";
+			int year = 0;
+			int numberOfRecords = Integer.parseInt(String.valueOf(in.readLine().charAt(0)));
+			int numberOfGenre = 0;
 
+			for (int i = 0; i < numberOfRecords; i++) {
+				line = in.readLine();
+				var ArtistTitleYear= line.split(";");
+				artist = ArtistTitleYear[0];
+				title = ArtistTitleYear[1];
+				year = Integer.parseInt(ArtistTitleYear[2]);
+				numberOfGenre = Integer.parseInt(in.readLine());
+				Set<String> genre = new HashSet<>(numberOfGenre);
+
+				for (int n = 0; n < numberOfGenre; n++) {
+					genre.add(in.readLine());
+				}
+
+				recordings.add(new Recording(artist, title, year, genre));
+
+			}
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	//importerar försäljningsdata från filen som ges som parameter (i
